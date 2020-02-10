cb43c26f1a44bbc993345314d12a9ca7c2de772d
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-02-10 16:02:31 -0500

    projects are successfully rendered to DOM

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 class App {
     constructor() {
         this.adapter = new Adapter()
+        this.createProjects = this.createProjects.bind(this)
+        this.addProjects = this.addProjects.bind(this)
     }
-    
+
     attachEventListeners() {
         document.querySelector(".projects-list").addEventListener("click", e => {
             const id = e.target.dataset.id
@@ -10,4 +12,19 @@ class App {
             console.log(project)
         })
     }
+
+    createProjects(projects) {
+        projects.data.forEach(project => {
+            new Project(project)
+        })
+        this.addProjects()
+    }
+
+    addProjects() {
+        document.querySelector(".projects-list").innerHTML = ''
+        Project.all.forEach(project => {
+            document.querySelector(".projects-list").innerHTML += project.renderProject()
+        })
+    }
+
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     const app = new App()
     app.attachEventListeners()
-    app.adapter.fetchProjects()
+    app.adapter.fetchProjects().then(app.createProjects)
 });
